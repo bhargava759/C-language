63ad2eb7bd52819c5826cfcb7e38b5f7965fcda5
bhargava759 <118883869+bhargava759@users.noreply.github.com>
2023-04-14 07:51:25 +0530

    Add files via upload

---
+++ C language assignment 3 day-3 sem-3/A3Q1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+   int num1,num2,i,j,flag;
+   printf("Enter the two intervals:");
+   scanf("%d %d",&num1,&num2);
+   printf("Prime Numbers between %d and %d:",num1,num2);
+   for(i=num1+1;i<num2;i++){
+      flag=0;
+      for(j=2;j<=i/2;++j){ 
+         if(i%j==0){
+            flag=1;
+            break;
+         }
+      }
+      if(flag==0)
+         printf("%d\n",i);
+   }
+   return 0;
+}
